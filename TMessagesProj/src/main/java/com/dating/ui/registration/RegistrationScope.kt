8673603a912b373dbaa07b55cbbfdd538972ebfc
Dmitiry Akishin <dakishin@gmail.com>
2018-03-01 16:24:41 +0300

    Регистрация 1

---
+++ TMessagesProj/src/main/java/com/dating/ui/registration/RegistrationScope.kt
@@ -0,0 +1,10 @@
+package com.dating.ui.registration
+
+import javax.inject.Scope
+
+/**
+ *   Created by dakishin@gmail.com
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RegistrationScope
---
+++ TMessagesProj/src/main/java/com/dating/ui/registration/RegistrationViewModel.kt
@@ -0,0 +1,9 @@
+package com.dating.ui.registration
+
+/**
+ *   Created by dakishin@gmail.com
+ */
+data class RegistrationViewModel(var isLoading: Boolean = false,
+                                 var hasError: Boolean = false
+
+)
